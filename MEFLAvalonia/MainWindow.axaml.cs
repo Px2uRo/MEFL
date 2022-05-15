83a26f478b393b875ebaa54a42eae77bb0e626e9
粉猫PinKcat <hongyu233@outlook.com>
2022-05-15 22:22:29 +0800

    你好世界

---
+++ MEFLAvalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MEFL
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
